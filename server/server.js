8287fd7c4dff605241c179b4f057c7b01ddad5a2
SilviaRbgl <silviarbgl@gmail.com>
2023-02-23 11:43:41 +0100

    feat: server configuration

--- server/server.js
+++ server/server.js
@@ -1,16 +1,50 @@
 import express from "express";
 import cors from "cors";
+import router from "./routes/test.js";
+import * as dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-app.use(cors());
+const mongoDBConnection = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.DB);
+    console.log("MongoDB is running in port", port);
+  } catch (error) {
+    console.log("error connecting to MongoDB", error);
+  }
+};
 
-app.listen(port, () => {
-  console.log("Server is running on " + port + "port");
-});
+const addMiddlewares = () => {
+  app.use(express.json());
+  app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
+  const corsOptions = {
+    // origin: "vercel deployment",
+    origin: "http://localhost:3000",
+    credentials: true,
+  };
+  app.use(cors());
+};
+
+const loadRoutes = () => {
+  app.use("/api", router);
+};
+
+const startServer = () => {
+  app.listen(port, () => {
+    console.log("Server is running in port" + port);
+  });
+};
+
+(async function controller() {
+  await mongoDBConnection();
+  addMiddlewares();
+  loadRoutes();
+  startServer();
+})();
