57a7dd81990efafa9b73e066c0eedfbfb572a140
SilviaRbgl <silviarbgl@gmail.com>
2023-03-27 10:04:59 +0200

    styling frontend

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      {" "}
+      <footer className="footer">
+        <p>SILVIA RBGL</p>
+        <p>
+          If you want to report a problem, you can better call Saul or
+          write me an <a href="mailto:silviarbgl@gmail.com">email.</a>
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
--- client/src/views/Homepage.tsx
+++ client/src/views/Homepage.tsx
@@ -1,58 +1,101 @@
 import React from "react";
+import Footer from "../components/Footer";
 
 function Homepage() {
   return (
     <>
       <div className="box">
-        <h1>Welcome to the Breaking Bad API Documentation</h1>
+        <h1>Welcome to the breakingbad.API</h1>
       </div>
-      <div className="box">
-        <h3>characters</h3>
-        <p>Characters objects can have the following properties:</p>
-        <p>(Arrays may contain only one element)</p>
+      <div className="container">
+        <h2>introduction</h2>
+        <p>
+          Welcome to the breakingbad.API! This documentation should help
+          familiarise yourself with the resources available and how to consume
+          them with HTTP requests.
+        </p>
+        <p>What is a REST API?</p>
+        <p>
+          An API is an application programming interface - in short, it’s a set
+          of rules and instructions that lets programs talk to each other,
+          exposing data and functionality across the internet in a consistent
+          format.
+        </p>
+        <p>
+          REST stands for Representational State Transfer. This is an
+          architectural pattern that describes how distributed systems can
+          expose a consistent interface. When people use the term ‘REST API,’
+          they are generally referring to an API accessed via HTTP protocol at a
+          predefined set of URLs.
+        </p>
+        <p>
+          These URLs represent various resources - any information or content
+          accessed at that location, which can be returned as JSON, HTML, audio
+          files, or images. Often, resources have one or more methods that can
+          be performed on them over HTTP, like GET, POST, PUT and DELETE.
+        </p>
+        <p>The breakingbad.API only allows you to GET data.</p>
+      </div>
+      <div className="container">
+         <h2>characters</h2>
+        <p>In this request you will find a list of the characters of the serie Breaking Bad.
+          Characters objects can have the following properties:</p>
         <ul>
           <li>
             <p>char_id: the general id of the character (Number)</p>
           </li>
           <li>
-            <p>name: the general id of the character (String)</p>
+            <p>name: the name of the character (String)</p>
           </li>
           <li>
-            <p>birthday: the general id of the character (String)</p>
+            <p>birthday: the date of the birthday of the character (String)</p>
           </li>
           <li>
-            <p>occupation: the general id of the character (Array)</p>
+            <p>occupation: the ocupation of the character (Array)</p>
           </li>
           <li>
-            <p>img: the general id of the character (String)</p>
+            <p>img: an image of the character (String)</p>
           </li>
           <li>
-            <p>status: the general id of the character (String)</p>
+            <p>status: the current life status of the character (String)</p>
           </li>
           <li>
-            <p>nickname: the general id of the character (String)</p>
+            <p>nickname: the nickname of the character (String)</p>
           </li>
           <li>
-            <p>appearance: the general id of the character (Array)</p>
+            <p>
+              appearance: the seasons in which the character appears (Array)
+            </p>
           </li>
           <li>
-            <p>portrayed: the general id of the character (String)</p>
+            <p>
+              portrayed: the name of the actor/actress of the character (String)
+            </p>
           </li>
           <li>
-            <p>category: the general id of the character (String)</p>
+            <p>category: the name of the serie (String)</p>
           </li>
           <li>
             {" "}
             <p>
-              better_call_saul_appearance: the general id of the character
+              better_call_saul_appearance: the appearence in "Better Call Saul"
               (Array)
             </p>
           </li>
         </ul>
       </div>
-      {/* <div className="box">
-        <h3>endpoints</h3>
-      </div> */}
+       
+ 
+      <div className="container">
+        <h2>endpoints</h2>
+        <h3>/api/all</h3>
+        <p>
+          The "/all" endpoint allows you to fetch the complete list of the
+          characters.
+        </p>
+        <p>http://localhost:5000/api/characters/all</p>
+      </div>
+    <Footer />
     </>
   );
 }
